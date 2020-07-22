55f402739ab2ae152bb24a699dc1be5537acd32d
Anand Chitipothu <anandology@gmail.com>
2020-07-22 19:01:13 +0000

    Implemented HyperProphetEngine.
    The HyperProphetEngine computes the forecast using the hyperprophet
    cloud service.
    
    Also added a setup method to initialize credentials globally.

--- hyperprophet/__init__.py
+++ hyperprophet/__init__.py
@@ -1,1 +1,10 @@
+"""
+    hyperprophet
+    ~~~~~~~~~~~~
+
+    The easiest way to run Prophet at Scale!
+"""
 from .forecaster import Prophet # noqa
+from .engines import setup # noqa
+
+__version__ = "0.1.0"
--- hyperprophet/engines.py
+++ hyperprophet/engines.py
@@ -5,6 +5,11 @@ hyperprophet.engines
 Compute engines for Prophet.
 """
 import pandas as pd
+import requests
+import os
+import tempfile
+import zipfile
+import time
 
 ENGINES = {}
 def make_engine(engine=None):
@@ -13,9 +18,9 @@ def make_engine(engine=None):
     if isinstance(engine, str):
         if engine not in ENGINES:
             raise ValueError(f"Invaid Engine: {engine!r}")
-        return ENGINES[engine]
+        return ENGINES[engine]()
 
-    return ENGINES['default']
+    return ENGINES['default']()
 
 def register_engine(name, engine):
     """Registers a new engine.
@@ -59,7 +64,6 @@ class LocalEngine(BaseEngine):
     """Forecast locally using Prophet.
     """
     def forecast(self, df_fit, df_predict, options):
-        keys = df_fit['key'].unique()
         df_fit_parts = dict(iter(df_fit.groupby('key')))
         df_predict_parts = dict(iter(df_predict.groupby('key')))
 
@@ -87,7 +91,168 @@ class LocalEngine(BaseEngine):
         forecast['key'] = key
         return forecast[columns]
 
+DEFAULT_ENDPOINT_URL = "https://api.hyperprophet.com"
+DEFAULT_API_TOKEN = None
 
-register_engine('zero', ZeroEngine())
-register_engine('local', LocalEngine())
-register_engine('default', LocalEngine())
+def setup(api_token, endpoint_url=None):
+    """Setup the default credentials for the hyperprophet service.
+
+    These credentials will be used when api_token is not explictly
+    specfied when creating HyperprophetEngine or Prophet instance.
+
+    Parameters
+    ----------
+    api_token:
+        API token to access the hyperprophet service
+
+    endpiont_url:
+        Optional endpoint URL to specify when woring against a different server.
+
+    Example
+    -------
+
+    from hyperprophet import setup
+    setup(api_token="0ef097a52c0942............e8a7d6")
+    """
+    global DEFAULT_API_TOKEN, DEFAULT_ENDPOINT_URL
+    DEFAULT_API_TOKEN = api_token
+
+    if endpoint_url is not None:
+        DEFAULT_ENDPOINT_URL = endpoint_url
+
+class HyperprophetEngine(BaseEngine):
+    """Engine to run forecast on the hyperprophet cloud.
+    """
+    def __init__(self, api_token=None, endpoint_url=None):
+        self.api_token = api_token or DEFAULT_ENDPOINT_URL
+        if self.api_token is None:
+            raise ValueError("Please provide api_token. You can also call the setup function to set it globally.")
+
+        self.endpoint_url = endpoint_url or DEFAULT_ENDPOINT_URL
+        self.endpoint_url = self.endpoint_url.rstrip("/")
+
+    def request(self, method, path, json=None, **kwargs):
+        headers = {"Authorization": "Bearer " + self.api_token}
+        url = self.endpoint_url + path
+        return requests.request(
+            method=method,
+            url=url,
+            headers=headers,
+            json=json,
+            **kwargs)
+
+    def forecast(self, df_fit, df_predict, options):
+        job = Job.create(self)
+        job.upload_files(df_fit, df_predict)
+        job.start(options)
+        job.wait()
+        return job.read_results_df()
+
+class Job:
+    def __init__(self, engine, id, status, data_upload_url=None, results_url=None, progress=0.0):
+        self.engine = engine
+        self.id = id
+        self.status = status
+        self.data_upload_url = data_upload_url
+        self.results_url = results_url
+        self.progress = progress
+
+    def start(self, options):
+        """Starts the job execution.
+
+        This must be called after uploading the files.
+        """
+        url = "/jobs.start"
+        payload = {
+            "id": self.id,
+            "options": options
+        }
+        response = self.engine.request("POST", url, json=payload)
+        if response.status_code != 200:
+            raise EngineError("Failed to upload the job payload. ({} - {})".format(response.status_code, response.text[:100]))
+
+        d = response.json()
+        if d['ok'] is False:
+            raise EngineError("Failed to upload the job payload. ({})".format(d['error']))
+
+        self._update(d['job'])
+
+    def _update(self, data):
+        """Updates the job with fresh details from the API.
+        """
+        self.status = data['status']
+        self.progress = data['progress']
+
+    def _refresh(self):
+        response = self.engine.request("GET", "/jobs.info", params={"id": self.id})
+        d = response.json()
+        if d['ok'] is False:
+            raise EngineError("Failed to get jon status. ({})".format(d['error']))
+        self._update(d['job'])
+
+    def read_results_df(self):
+        response = self.engine.request("GET", "/jobs.result", params={"id": self.id})
+        if response.status_code != 200:
+            raise EngineError("Failed to read the results. ({} - {})".format(response.status_code, response.text[:100]))
+        d = response.json()
+        if d['ok'] is False:
+            raise EngineError("Failed to read the results. ({})".format(d['error']))
+
+        return pd.read_parquet(d['download_url'])
+
+    def upload_files(self, df_train, df_predict):
+        """Uploads the required files to the job.
+        """
+        with tempfile.TemporaryDirectory() as tmp:
+            train_path = os.path.join(tmp, "train.parq")
+            df_train.to_parquet(train_path)
+
+            predict_path = os.path.join(tmp, "predict.parq")
+            df_predict.to_parquet(predict_path)
+
+            zip_path = os.path.join(tmp, "payload.zip")
+            with zipfile.ZipFile(zip_path, "w") as z:
+                z.write(train_path, "train.parq")
+                z.write(predict_path, "predict.parq")
+
+            headers = {
+                "content-type": "application/zip"
+            }
+            response = requests.put(
+                self.data_upload_url,
+                data=open(zip_path, 'rb'),
+                headers=headers)
+            if response.status_code != 200:
+                raise EngineError("Failed to upload the job payload. ({} - {})".format(response.status_code, response.text[:100]))
+
+    @classmethod
+    def create(cls, engine):
+        response = engine.request("POST", "/jobs.create")
+        if response.status_code != 200:
+            raise EngineError("Failed to create a new job. ({} - {})".format(response.status_code, response.text[:100]))
+
+        d = response.json()
+        if not d['ok']:
+            raise EngineError("Failed to create a new job. ({})".format(d['error']))
+
+        job = d['job']
+
+        return Job(
+            engine=engine,
+            id=job['id'],
+            status=job['status'],
+            data_upload_url=job['data_upload_url'])
+
+    def wait(self):
+        while self.status not in ['SUCCESS', 'FAILED', 'ABORTED']:
+            self._refresh()
+            print("[JOB {}] status={} progress={}".format(self.id, self.status, self.progress))
+            time.sleep(5)
+
+class EngineError(Exception):
+    pass
+
+register_engine('zero', ZeroEngine)
+register_engine('local', LocalEngine)
+register_engine('remote', HyperprophetEngine)
+register_engine('default', HyperprophetEngine)
